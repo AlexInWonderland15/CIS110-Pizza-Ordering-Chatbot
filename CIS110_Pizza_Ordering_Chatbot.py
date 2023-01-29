8e57a9db7924be6c8d3f07b7be157817ea69a9ad
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-29 12:31:31 -0800

    Added Conditional Logic

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,9 +1,12 @@
-print("Hello, my name is Alex your virtual assistant. I will help you order a pizza!")
+print("Hello, my name is Alex! I am your virtual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press Enter.")
 
 userName = input("\nEnter your name:  ")
 
-print(f"\nHello, {userName}. Nice to meet you!")
+if userName.lower() == "alex crider":
+    print(f"\nMy creator, {userName}. Happy to help you!")
+else:
+    print(f"\nYo, {userName}! Nice to meet you!")
 
 size = input("\nWhat size do you want? Enter small, medium, or large:  ")
 flavor = input("\nEnter the flavor of pizza:  ")
@@ -12,11 +15,26 @@ quantity = input("\nHow many of these do you want to order? Enter a numeric valu
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery?:  ")
 
+if method.lower() == "delivery":
+    deliveryFee = 5
+else:
+    deliveryFee = 0
+
 salesTax = 1.1
-pizzaCost = 14.99
-total = (pizzaCost * quantity) * salesTax
+if size.lower() == "small":
+    pizzaCost = 8.99
+elif size.lower() == "medium":
+    pizzaCost = 14.99
+elif size.lower() == "large":
+    pizzaCost = 17.99
+total = (pizzaCost * quantity) * salesTax + deliveryFee
 
 
 print("-" * 10)
-print(f"Thank you, {userName}, for your order.")
-print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
+print(f"Sick! Thanks, {userName}, for your order.")
+print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}. Cool? Cool.")
+if total >= 50:
+    print("\nCongratulations! You've been awarded a coupon for $10 off your next order.")
+else:
+    print("\nOrders over $50 will receive a coupon for $10 off!")
+print("-" * 10)
