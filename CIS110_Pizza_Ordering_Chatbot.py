9549d2e675c64750473ba42f476d23320a2acd28
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-02-02 17:19:08 -0800

    Added Data Validation

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -2,6 +2,8 @@ print("Hello, my name is Alex! I am your virtual assistant. I will help you orde
 print("I am going to ask you a few questions. After typing an answer, press Enter.")
 
 userName = input("\nEnter your name:  ")
+while len(userName) == 0:
+    userName = input("Hey, friend. Name cannot be blank! Please enter your name: ")
 
 if userName.lower() == "alex crider":
     print(f"\nMy creator, {userName}. Happy to help you!")
@@ -9,12 +11,27 @@ else:
     print(f"\nYo, {userName}! Nice to meet you!")
 
 size = input("\nWhat size do you want? Enter small, medium, or large:  ")
-flavor = input("\nEnter the flavor of pizza:  ")
-crustType = input("\nWhat type of crust do you want?:  ")
-quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
-quantity = int(quantity)
-method = input("\nIs this carry out or delivery?:  ")
+while size.lower() not in ["small", "medium", "large" ]:
+    size = input("Yo! Invalid value! Please enter small, medium or large: ")
+while len(size) == 0:
+    size = input("Hey, pal. Size cannot be blank! Please enter a size: ")
 
+flavor = input("\nEnter the flavor of pizza:  ")
+while len(flavor) == 0:
+    flavor = input("Hey, buddy. Flavor cannot be blank! Please enter a flavor: ")
+
+crustType = input("\nWhat type of crust do you want?:  ")
+while len(crustType) == 0:
+    crustType = input ("Hey, comrade. Crust type cannot be blank! Please enter a crust type: ")
+
+quantity = input("\nHow many of these do you want to order? Enter a numeric value:  ")
+while not quantity.isdigit():
+    quantity = input("Hey, dude. Value not recognized. Please enter a numeric value: ")
+quantity = int(quantity)
+
+method = input("\nIs this carry out or delivery?:  ")
+while method not in ["carry out", "delivery"]:
+    method = input("Hey, partner. Invalid value! Please enter carry out or delivery: ")
 if method.lower() == "delivery":
     deliveryFee = 5
 else:
@@ -31,7 +48,7 @@ total = (pizzaCost * quantity) * salesTax + deliveryFee
 
 
 print("-" * 10)
-print(f"Sick! Thanks, {userName}, for your order.")
+print(f"Sick! Thanks for your order, {userName}!")
 print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}. Cool? Cool.")
 if total >= 50:
     print("\nCongratulations! You've been awarded a coupon for $10 off your next order.")
